46e380cf9bced4ded201b477802863c43ca83ae9
Михаил Сергеев <server-15@yandex.ru>
2023-05-24 03:32:34 +0500

    аннотации

--- blabla_user/api/utils.py
+++ blabla_user/api/utils.py
@@ -1,8 +1,9 @@
 from django.core.files import File
 from pydub import AudioSegment
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-def convert_to_mp3(audio_file, content_type='audio/mpeg'):
+def convert_to_mp3(audio_file: InMemoryUploadedFile, content_type='audio/mpeg') -> File:
     mp3_converted_file = AudioSegment.from_wav(audio_file)
     new_path = audio_file.name[:-3] + 'mp3'
     mp3_converted_file.export(new_path, bitrate="192k")
